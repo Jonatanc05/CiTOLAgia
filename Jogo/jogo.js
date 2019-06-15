1d65c67c927323b012cdbba3b85355200cf285bd
WagnerACV <40211958+WagnerACV@users.noreply.github.com>
2019-06-15 13:42:34 -0300

    Add files via upload

--- Jogo/jogo.js
+++ Jogo/jogo.js
@@ -1,7 +1,6 @@
 let root = document.getElementById('divGrade');
 
 var verif = 0,contador = 0; // Variável para ver numero de cartas viradas
-/*    Criação Cartões    */
 
 let cooldown = 200; // Tempo para clicar novamente, em ms
 let canClick = true; // Se a carta pode ser clicada ou não
@@ -57,116 +56,118 @@ let arrOrganelas = [
     }
 ];
 
-// Criação do vetor de cartas
-let arrCartao = [];
-arrDiv = [];
-arrDiv[0]=arrDiv[1]=null;
-let arrLogc = [];
-arrLogc[0]=arrLogc[1]=null;
+newgame();//começa o jogo quando abre a página
 
-for(i = 0; i < arrOrganelas.length*2; i+=2){ // Atribui a imagem de uma organela às cartas ímpares e a descrição sobre ela às cartas pares
-    arrCartao[i] = { virada: false, img: "../imgs/organelas/"+arrOrganelas[i/2].Nome+".png", descricao: undefined, Nome: arrOrganelas[i/2].Nome, id : 'carta'+i };
-    arrCartao[i+1] = { virada: false, img: undefined, descricao: arrOrganelas[i/2].Descricao, Nome: arrOrganelas[i/2].Nome ,id : 'carta'+(i+1)};
-}
+function newgame(){
+    // Criação do vetor de cartas
+    let arrCartao = [];
+    arrDiv = [];
+    arrDiv[0]=arrDiv[1]=null;
+    let arrLogc = [];
+    arrLogc[0]=arrLogc[1]=null;
 
-for(i = 0; i < 24; i++){ // Randomiza a posição das cartas
-    let idx1 = Math.floor(Math.random()*100) % 24;
-    let idx2 = Math.floor(Math.random()*100) % 24;
-    temp = arrCartao[ idx1 ];
-    arrCartao[ idx1 ] = arrCartao[ idx2 ];
-    arrCartao[ idx2 ] = temp;
-}
-
-for(i = 0; i < 24; i++){ // Cria uma div para cada carta
-    let el = document.createElement('div');
-    //let id = document.createAttribute('id');
-    el.setAttribute('id', 'carta'+i);
-    el.setAttribute('class', 'elemento');
-    el.setAttribute('alt', arrCartao[i].Nome);
-    if(arrCartao[i].img == undefined){ // Caso a carta em questão seja de descrição, cria um 'p' e o coloca dentro da div respectiva da carta
-        let p = document.createElement('p');
-        p.innerText = arrCartao[i].descricao;
-        if( !arrCartao[i].virada)
-            p.style.display = "none";
-        el.appendChild(p);
+    for(i = 0; i < arrOrganelas.length*2; i+=2){ // Atribui a imagem de uma organela às cartas ímpares e a descrição sobre ela às cartas pares
+        arrCartao[i] = { virada: false, img: "../imgs/organelas/"+arrOrganelas[i/2].Nome+".png", descricao: undefined, Nome: arrOrganelas[i/2].Nome, id : 'carta'+i };
+        arrCartao[i+1] = { virada: false, img: undefined, descricao: arrOrganelas[i/2].Descricao, Nome: arrOrganelas[i/2].Nome ,id : 'carta'+(i+1)};
     }
-    else{ // Caso seja uma imagem, atribui ela à carta
-        el.style.backgroundImage = "url(../imgs/verso.jpg)";
+
+    for(i = 0; i < 24; i++){ // Randomiza a posição das cartas
+        let idx1 = Math.floor(Math.random()*100) % 24;
+        let idx2 = Math.floor(Math.random()*100) % 24;
+        temp = arrCartao[ idx1 ];
+        arrCartao[ idx1 ] = arrCartao[ idx2 ];
+        arrCartao[ idx2 ] = temp;
     }
-    el.addEventListener("click", function(e){
-        let id = e.currentTarget.getAttribute('id').substr(5);
 
-        //console.log(id);
-        //console.log(arrCartao[id]);
-        if(arrCartao[id].virada == false){
-            arrCartao[id].virada = true;
-            verif = 0;
-            if(arrCartao[id].img == undefined){ // Quando é descrição
-                let par = e.currentTarget.children[0];
-                par.style.display = "block";
-                e.currentTarget.style.backgroundImage = "url(../imgs/text-background.jpg)";
-            } 
-            else{
-                e.currentTarget.style.backgroundImage = "url("+arrCartao[id].img+")";
-            }
+    for(i = 0; i < 24; i++){ // Cria uma div para cada carta
+        let el = document.createElement('div');
+        el.setAttribute('id', 'carta'+i);
+        el.setAttribute('class', 'elemento');
+        el.setAttribute('alt', arrCartao[i].Nome);
+        if(arrCartao[i].img == undefined){ // Caso a carta em questão seja de descrição, cria um 'p' e o coloca dentro da div respectiva da carta
+            let p = document.createElement('p');
+            p.innerText = arrCartao[i].descricao;
+            if( !arrCartao[i].virada)
+                p.style.display = "none";
+            el.appendChild(p);
         }
-        else verif = 1;
-
-        /*    Lógica do jogo      */
-        if(arrLogc[0] == null && verif == 0){
-            arrLogc[0] = arrCartao[id];
-            arrDiv[0] = e.currentTarget;
-
-        }else if(arrLogc[1] == null && verif == 0){
-            arrLogc[1] = arrCartao[id];
-            arrDiv[1] = e.currentTarget;
-
-            if(arrLogc[0].Nome == arrLogc[1].Nome){
-                console.log("Acertou!!!!");
-                contador++;
-                arrDiv[0] = arrLogc[1] = null;
-                arrDiv[1] = arrLogc[0] = null;
-                if(contador==12){
-                    window.alert("Venceu o game");
-                    contador=0;
-                }
-            }else{
-                arrLogc[0].virada = arrLogc[1].virada = false;
-                
-                if(arrLogc[0].img == undefined){
-                    setTimeout( function(){
-                        arrDiv[0].style.backgroundImage = "url(../imgs/verso.jpg)";
-                        let par = arrDiv[0].children[0];
-                        par.style.display = "none";
-                    }, 800);
-                }else{
-                    setTimeout( function(){
-                        arrDiv[0].style.backgroundImage = "url(../imgs/verso.jpg)";
-                    }, 800);
-                }
-
-                if(arrLogc[1].img == undefined){
-                    setTimeout( function(){
-                        arrDiv[1].style.backgroundImage = "url(../imgs/verso.jpg)";
-                        let par = arrDiv[1].children[0];
-                        par.style.display = "none";
-                        arrLogc[0] = arrLogc[1] = null;
-                        arrDiv[1] = arrDiv[0] = null;
-                    }, 800);
-                }else{
-                    setTimeout( function(){
-                        arrDiv[1].style.backgroundImage = "url(../imgs/verso.jpg)";
-                        arrLogc[0] = arrLogc[1] = null;
-                        arrDiv[1] = arrDiv[0] = null;
-                    }, 800);
-                }
-                
-            }
+        else{ // Caso seja uma imagem, atribui ela à carta
+            el.style.backgroundImage = "url(../imgs/verso.jpg)";
         }
-    });
-    root.appendChild(el);
+        el.addEventListener("click", function(e){
+            let id = e.currentTarget.getAttribute('id').substr(5);
+
+            //console.log(id);
+            //console.log(arrCartao[id]);
+            if(arrCartao[id].virada == false){
+                arrCartao[id].virada = true;
+                verif = 0;
+                if(arrCartao[id].img == undefined){ // Quando é descrição
+                    let par = e.currentTarget.children[0];
+                    par.style.display = "block";
+                    e.currentTarget.style.backgroundImage = "url(../imgs/text-background.jpg)";
+                } 
+                else{
+                    e.currentTarget.style.backgroundImage = "url("+arrCartao[id].img+")";
+                }
+            }
+            else verif = 1;
+
+            /*    Lógica do jogo      */
+            if(arrLogc[0] == null && verif == 0){
+                arrLogc[0] = arrCartao[id];
+                arrDiv[0] = e.currentTarget;
+
+            }else if(arrLogc[1] == null && verif == 0){
+                arrLogc[1] = arrCartao[id];
+                arrDiv[1] = e.currentTarget;
+
+                if(arrLogc[0].Nome == arrLogc[1].Nome){
+                    console.log("Acertou!!!!");
+                    contador++;
+                    arrDiv[0] = arrLogc[1] = null;
+                    arrDiv[1] = arrLogc[0] = null;
+                    if(contador==12){
+                        window.alert("Venceu o game");
+                        contador=0;
+                    }
+                }else{
+                    arrLogc[0].virada = arrLogc[1].virada = false;
+                    
+                    if(arrLogc[0].img == undefined){
+                        setTimeout( function(){
+                            arrDiv[0].style.backgroundImage = "url(../imgs/verso.jpg)";
+                            let par = arrDiv[0].children[0];
+                            par.style.display = "none";
+                        }, 800);
+                    }else{
+                        setTimeout( function(){
+                            arrDiv[0].style.backgroundImage = "url(../imgs/verso.jpg)";
+                        }, 800);
+                    }
+
+                    if(arrLogc[1].img == undefined){
+                        setTimeout( function(){
+                            arrDiv[1].style.backgroundImage = "url(../imgs/verso.jpg)";
+                            let par = arrDiv[1].children[0];
+                            par.style.display = "none";
+                            arrLogc[0] = arrLogc[1] = null;
+                            arrDiv[1] = arrDiv[0] = null;
+                        }, 800);
+                    }else{
+                        setTimeout( function(){
+                            arrDiv[1].style.backgroundImage = "url(../imgs/verso.jpg)";
+                            arrLogc[0] = arrLogc[1] = null;
+                            arrDiv[1] = arrDiv[0] = null;
+                        }, 800);
+                    }
+                    
+                }
+            }
+        });
+        root.appendChild(el);
+    }
 }
-
 
 /*    Configuração e estilização   */
 
@@ -200,30 +201,8 @@ voltarBtn.addEventListener("mouseenter", function(e){e.currentTarget.setAttribut
 voltarBtn.addEventListener("mouseleave", function(e){e.currentTarget.setAttribute('src', '../imgs/aprender-conteudo.png');});
 voltarBtn.addEventListener("click", function(e){window.location.href = "../index.html";});
 
-let recBtn = document.getElementById('recarrega');
-/*recBtn.addEventListener("click", function(){
-    var el = document.getElementById('carta'+i);
-
-    for(i = 0; i < 24; i++){
-        if(arrCartao[el.getAttribute('id').substr(5)].img == undefined1){
-            el.style.backgroundImage = "url(../imgs/verso.jpg)";
-            el.children[0].style.display = "none";
-        }
-        else
-            el.style.backgroundImage = "url(../imgs/verso.jpg)";
-        
-    }
-
-    for(i = 0; i < arrOrganelas.length*2; i+=2){ // Atribui a imagem de uma organela às cartas ímpares e a descrição sobre ela às cartas pares
-        arrCartao[i] = { virada: false, img: "../imgs/organelas/"+arrOrganelas[i/2].Nome+".png", descricao: undefined, Nome: arrOrganelas[i/2].Nome, id : 'carta'+i };
-        arrCartao[i+1] = { virada: false, img: undefined, descricao: arrOrganelas[i/2].Descricao, Nome: arrOrganelas[i/2].Nome ,id : 'carta'+(i+1)};
-    }
-    
-    for(i = 0; i < 24; i++){ // Randomiza a posição das cartas
-        let idx1 = Math.floor(Math.random()*100) % 24;
-        let idx2 = Math.floor(Math.random()*100) % 24;
-        temp = arrCartao[ idx1 ];
-        arrCartao[ idx1 ] = arrCartao[ idx2 ];
-        arrCartao[ idx2 ] = temp;
-    }
-});*/
+let recBtn = document.getElementById('recarrega'); //newgame
+recBtn.addEventListener("click", function(){
+    root.innerHTML = "";
+    newgame();
+});
